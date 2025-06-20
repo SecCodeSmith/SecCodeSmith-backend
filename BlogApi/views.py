38a97b80b4a3cabf64c23210fdf4b259bcd8f965
SecCodeSmith <admin@seccodesmith.pl>
2025-06-20 17:03:12 +0200

    Fix tag search 2

--- BlogApi/views.py
+++ BlogApi/views.py
@@ -72,11 +72,12 @@ class PostPageViewEndpoint(APIView):
             if filt_json:
                 filt_json = json.loads(filt_json)
 
-                if 'title' in filt_json:
+                if 'title' in filt_json and filt_json['title'] != '':
                     posts = posts.filter(title__icontains=filt_json['title'])
                 if 'tags' in filt_json:
-                    posts = posts.filter(tags__slug__in=filt_json['tags'])
-                if 'category' in filt_json:
+                    for slug in filt_json['tags']:
+                        posts = posts.filter(tags__slug=slug)
+                if 'category' in filt_json and filt_json['category'] != '':
                     posts = posts.filter(category__slug=filt_json['category'])
 
 
